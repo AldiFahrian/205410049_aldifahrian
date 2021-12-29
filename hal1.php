aa9fc309b4b868661dd44c6fd42e73b75a4d412c
AldiFahrian <92723865+AldiFahrian@users.noreply.github.com>
2021-12-29 21:46:07 +0700

    Add files via upload

---
+++ hal1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT di masa depan</title>
+</head>
+<body>
+    <h1>
+    Bagaimana Peran Teknologi Informasi pada Dunia Kerja di Masa Depan? 
+    </h1>
+    <p>
+        Semua orang yang hidup di zaman sekarang sama-sama sedang menuju kehidupan di masa depan. Ini adalah kehidupan yang mungkin bisa saja lebih hebat dan canggih jika dibandingkan dengan kehidupan era digital seperti saat ini. Ketika perkembangan Teknologi Informasi datang dan menyentuh organisasi, perusahaan dan tempat-tempat kerja lainnya, maka transformasi akan terjadi secara eksponensial, baik dari visi, proses, teknologi, budaya kerja, bahkan praktik kerja itu sendiri. 
+ 
+Perubahan yang sangat cepat dan drastis seringkali menuntut perusahaan-perusahaan masa kini untuk bergerak dengan sangat cepat agar tidak tertinggal dengan para kompetitornya. Ini juga membuat perusahaan atau tempat kerja yang berorientasi masih sangat tradisional dengan susunan hierarki yang sangat mengikat menjadi semakin pudar ditelan masa. 
+ 
+Orang-orang modern yang hidup di zaman kini dan di masa depan akan jauh lebih menyukai perusahaan yang fleksibel, tidak rigid dan tidak terlalu mengikat, namun penuh dengan inovasi. Perusahaan-perusahaan yang berorientasi teknologi akan menjadi tempat kerja yang futuristik di dunia kerja masa depan. Tempat kerja seperti inilah yang sangat diinginkan oleh kita semua di masa yang akan mendatang. 
+ 
+Itu mungkin hanya sedikit bayangan tentang peran teknologi informasi di masa depan, sekarang mari kita membahas tentang keadaan dunia kerja di masa depan. Seperti apa saja tempat kerja yang futuristik atau yang bisa menjadi tempat kerja idaman di masa mendatang? Seperti apa peranan manajemen dan tenaga kerja pada tempat kerja masa depan nanti? Apakah menggunakan robot seutuhnya dan manusia tidak melakukan pekerjaan apapun? Atau, manusia masih menjalankan tugasnya untuk terus bekerja? Setelah itu, bagaimana bentuk peranan yang dimainkan oleh teknologi informasi di masa depan? 
+ 
+Kami yakin pasti ada banyak pertanyaan yang hadir di dalam benak kita semua jika berpikir tentang teknologi dan dunia kerja di masa depan. Memang sih, semua itu bisa saja akan berlangsung dalam waktu yang masih lama, sehingga kita merasa tidak perlu untuk memikirkannya sekarang. Akan tetapi, tidak ada salahnya untuk memprediksi dan merancang masa depan dengan teknologi informasi mulai dari sekarang, bukan? 
+    </p>
+    <p>1. Kelincahan, Dinamis, dan Hampir Seluruh Pekerja Berkomunikasi secara Virtual. 
+        Prediksi pertama yang kami miliki di sini adalah beberapa puluh tahun mendatang, hampir semua orang tidak memiliki waktu untuk berinteraksi langsung secara tatap muka dengan orang-orang di sekitarnya. Semua bisa berinteraksi melalui teknologi informasi tanpa terkecuali. Karakteristik kelincahan dan dinamis semakin menjadi-jadi di masa depan. Hampir tidak ada satu orang pun yang bisa bersikap santai, karena untuk bisa bertahan hidup dan memiliki ketahanan, semua orang dituntut untuk bergerak cepat </p>
+<p>2. Inovasi, Kreasi dan Gangguan (Disruptions) adalah Hal yang Sangat Normal. 
+    Inovasi, kreasi dan gangguan sudah bagaikan norma yang perlu ditaati dan tidak bisa dilanggar di masa depan. Di masa mendatang, semua orang akan bergerak dengan cepat, lincah dan dinamis seperti yang digambarkan pada poin pertama. Tapi, untuk apa kedinamisan semua itu? Tentu saja untuk terus berinovasi dan berkreasi. Teknologi informasi akan semakin canggih bahkan kecanggihannya berada di luar imajinasi kita. Jika kita tidak bisa menerima inovasi dan kreasi sebagai suatu hal yang normal, maka kita tidak bisa bertahan, apalagi untuk menjadi yang unggul dari yang lain. Gangguan-gangguan dari dalam diri dan luar yang mendorong kita untuk terus berinovasi adalah hal yang sangat normal terjadi di masa depan. </p>
+    <p>3. Selalu Terhubung selama 24/7 adalah Hal yang Wajar. 
+        Peranan teknologi informasi yang semakin hebat dan canggih di masa depan akan memungkinkan kita semua untuk bisa terhubung selama 24/7 atau setiap hari dalam seminggu non-stop, tanpa terkendala dengan lokasi geografis dan keberadaan fisik. Yap, non-stop! Seakan tidak ada lagi jeda untuk beristirahat. Jadi jangan kaget jika di masa mendatang, kita akan ditelepon untuk mengadakan rapat kerja di malam hari, atau melakukan conference call di pukul 3 pagi, wow!</p>
+<p>
+    3. Selalu Terhubung selama 24/7 adalah Hal yang Wajar. 
+    Peranan teknologi informasi yang semakin hebat dan canggih di masa depan akan memungkinkan kita semua untuk bisa terhubung selama 24/7 atau setiap hari dalam seminggu non-stop, tanpa terkendala dengan lokasi geografis dan keberadaan fisik. Yap, non-stop! Seakan tidak ada lagi jeda untuk beristirahat. Jadi jangan kaget jika di masa mendatang, kita akan ditelepon untuk mengadakan rapat kerja di malam hari, atau melakukan conference call di pukul 3 pagi, wow!
+</p>
+<p>
+    4. Pekerjaan Penuh Waktu dan Mobilitas ke Tempat Kerja akan Semakin Berkurang. 
+Dunia kerja di masa depan tidak lagi menawarkan tempat kerja dengan pekerjaan tradisional yang mengharuskan tenaga kerjanya untuk duduk diam bekerja di depan laptop selama 8-9 jam per hari. Masa depan menawarkan pekerjaan yang dinamis, sehingga menjadi freelancer jauh lebih diminati daripada pekerjaan penuh waktu di tempat kerja. Hal ini membuat frekuensi mobilitas seseorang dari rumah ke kantor akan semakin berkurang. Mereka bisa bekerja dimana saja tanpa harus pergi ke kantor. Semuanya menjadi sangat mudah, cepat dan ringan. Tidak ada lagi hambatan dalam berkoneksi, meskipun bekerja tim secara tatap muka akan menjadi hal yang sangat jarang. 
+</p>
+<p>
+    5. Transformasi Pekerjaan dan Keterampilan Kerja akan Terus Bertambah. 
+Apa yang dapat rekan pembaca lihat dari perkembangan pekerjaan dan keterampilan di masa kini dengan masa dahulu? Mungkin beberapa di antaranya adalah jenis profesi yang semakin bertambah, permintaan keterampilan kerja yang semakin bervariasi dan lain sebagainya. 
+ 
+Yap! semua itu juga akan terjadi di masa depan. Tentunya dengan perkembangan dan perubahan yang lebih cepat dan bervariasi daripada sekarang. Beberapa keterampilan seperti soft skill yang tidak ada pada waktu sekarang, mungkin akan tercipta di masa depan. Tidak hanya itu, kita dituntut untuk bisa memahaminya bahkan menguasainya. Jika tidak? Kita akan termasuk ke dalam kelompok yang tertinggal. 
+ 
+</p>
+<p>
+    6. Fokus pada Solusi Real-Time dan Tepat pada Sasaran. 
+Peranan teknologi informasi dan dunia kerja yang futuristik akan berfokus pada solusi real-time dan tepat pada sasaran, bukan pada praktik dan proses. Semakin meningkatnya ketersediaan dan ketergantungan pada informasi dan data, maka perusahaan-perusahaan di masa depan akan dijalankan pada platform digital yang aman dan terintegrasi penuh. Perkembangan digital juga memungkinkan para individu untuk menyelesaikan masalah secara langsung atau real time dengan solusi yang tepat sasaran. Seperti yang dikatakan oleh Satya Nadella bahwa:
+“Di masa depan, setiap jenis pekerjaan akan memiliki komponen digital. Namun, bukan berarti semua orang harus menjadi ilmuwan komputer”
+</p>
+    </body>
+</html>
---
+++ hal2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Teknologi IT Ini Akan Mempengaruhi Dunia Pendidikan Di Masa Depan</h1>
+    <p>
+        Teknologi IT saat ini berkembang begitu pesatnya, hingga kita semua tidak bias hidup tanpa menggunakan teknologi. Meskipun perkembangan teknologi tersebut banyak memberikan dampak negatif di dunia sosial, namun tidak sedikit pula penerapan teknologi tersebut yang berbuah sesuatu yang sangat positif bagi manusia.
+
+Salah satu bidang yang telah berevolusi berkat teknologi dengan pesat adalah dunia pendidikan. Ucapkan selamat tinggal pada cara mendidik yang konvensional dan kaku, karena beberapa penerapan teknologi informasi ini akan mengubah wajah pendidikan di Indonesia.
+    </p>
+    <p>
+        Menurut Fernando Uffe, seorang pakar pendidikan di Extramarks Indonesia, setidaknya ada 9 teknologi informasi yang akan mempengaruhi model pendidikan kita saat ini, yaitu:
+    </p>
+    <p>
+        Video Based Learning :
+    </p>
+    <p>
+        Jika selama lebih dari satu dekade, pembelajaran di sekolah didominasi dengan media ajar tulisan dan presentasi power point, maka kedepannya pembelajaran berbasis video akan semakin digemari. Hal tersebut dikarenakan model pola pikir anak di zaman sekarang yang cepat bosan apabila hanya disuguhkan tulisan atau gambar yang tidak bergerak. Maka sebuah video interaktif adalah jawabannya. Namun bukan rekaman guru yang mengajar, akan tetapi visualisasi dari materi atau konten mata pelajaran tersebut di dunia nyata.
+    </p>
+    <p>
+        Gamification : 
+    </p>
+    <p>
+        Perkembangan game online gratis yang sangat pesat dibarengi dengan harga paket data yang murah sehingga mampu dibeli oleh setiap anak disegala umur, membuat mereka cenderung kecanduan game dan kurang memperhatikan pelajaran. Karena itu metode gamification atau membawa sebuah permainan digital ke dalam mata pelajaran sangat mungkin dilakukan. Hal tersebut akan membuat siswa dapat menikmati pengalaman pembelajaran yang dibalut dalam sebuah aplikasi game yang menarik, diharapkan dengan gamification para siswa mampu secara terbuka  terlibat ke dalam aktifitas dan membuka diri terhadap kemungkinan belajar.
+    </p>
+    <p>
+        Big Data : 
+    </p>
+    <p>
+        Ucapkan selamat tinggal terhadap formulir kertas yang kuno, sekarang Big data dapat menampung segala macam data terkait sekolah dan pembelajaran secara digital, termasuk data siswa, guru, orang tua wali, sarana prasarana dan lain sebagainya. Data tersebut bisa terupdate dengan mudah dan dapat menganalisa data dengan baik sehingga mampu untuk memberikan solusi untuk proses pembelajaran siswa secara lebih efektif.
+    </p>
+    <p>
+        Cloud Computing : 
+    </p>
+    <p>
+        Jikalau di masa lampau semua data sekolah ditempatkan di lemari atau di gudang sekolah, maka sekarang semua data tersebut dapat disimpan secara online sehingga dapat menghemat tempat dan meminimalisir kerusakan data akibat tergerus waktu. Data yang diinginkan atau dibutuhkan dapat diambil dalam waktu yang sangat singkat dan tepat sasaran.
+    </p>
+    <p>
+        Internet Of Things : 
+    </p>
+    <p>
+        Dengan Internet of Things akan menghubungkan setiap device yang ada di sekolah, semua device siswa, guru dan penilik sekolah akan dapat terkoneksi secara bersama. Seluruh device yang meliputi keamanan, absensi, monitoring dan berbagai peralatan lainnya di sekolah akan dapat terhubung dan terkontrol dari satu ruang pusat.
+    </p>
+    <p>
+        Cyber Security :
+    </p>
+    <p>
+        Dengan adanya digitalisasi data di seluruh lingkungan sekolah. Kemanan data pribadi siswa, guru dan sekolah menjadi penting untuk diperhatikan. Karena itu dibutuhkan pengamanan digital yang canggih dilingkungan sekolah agar dapat menghondari penggunaan data yang tidak pada tempatnya oleh pihak yang tidak bertanggung jawab.
+    </p>
+</body>
+</html>
